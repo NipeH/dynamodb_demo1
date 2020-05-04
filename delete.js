134787ad58ea61a629791588b491fa36f13a5657
NipeH <nipe_h@hotmail.com>
2020-05-04 22:55:07 +0300

    first

---
+++ delete.js
@@ -0,0 +1,31 @@
+var AWS = require("aws-sdk");
+let awsConfig = {
+    "region": "us-east-2",
+    "endpoint": "http://dynamodb.us-east-2.amazonaws.com",
+    "accessKeyId": "AKIAIDHKWRBLMX4QOONQ", "seretAccessKey": "BcUpw7gygBFTaqiKb7mFYmIACUQZB/7cYPwNnwAQ"
+};
+
+
+AWS.config.update(awsConfig);
+
+let docClient = new AWS.DynamoDB.DocumentClient();
+
+let remove = function () {
+
+    var params = {
+        TableName: "users",
+        Key: {
+            "email_id": "example2@gmail.com"
+        }
+    };
+    docClient.delete(params, function (err, data) {
+
+        if (err) {
+            console.log("users::delete::error - " + JSON.stringify(err, null, 2));
+        } else {
+            console.log("users::delete::success");
+        }
+    });
+}
+
+remove();
